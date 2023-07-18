bf5658d7ae5957169877a6a108e5fe379128f895
itsdapi <altgr3@foxmail.com>
2023-07-18 11:46:58 +0800

    shrink the size of docker image

--- README.md
+++ README.md
@@ -5,7 +5,19 @@
 搭配RouterOS内置的脚本功能可以实现公网地址更新，而且不依赖于任何的第三方公网ip获取工具，有效避免梯子等原因导致无法获取正确ip，或者第三方网站暂时下线等原因
 
 ## 食用方法
-用写好的Docker-compose就能从源码编译并启动，后续我也会push到docker hub
+用写好的Docker-compose就能从源码编译并启动 或者从Docker-hub拉取
+
+```shell
+docker pull itsdapi/http-aliddns-server:latest
+```
+
+然后通过GET请求访问
+```
+http://your.ip:5000/updateV4?ip=1.1.1.1
+```
+
+将your.ip替换成服务器ip 有updateV4, updateV6可选 然后直接输入ip地址即可 <br>
+>我突然发现其实可以把域名啥的都放上来 先加到TODO罢^^
 
 ## 环境变量
 
--- src/aliddns.py
+++ src/aliddns.py
@@ -1,10 +1,10 @@
 from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
+# from aliyunsdkcore.acs_exception.exceptions import ClientException
+# from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkalidns.request.v20150109.DescribeSubDomainRecordsRequest import DescribeSubDomainRecordsRequest
-from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
-import requests
-from urllib.request import urlopen
+# from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+# import requests
+# from urllib.request import urlopen
 from flask import Flask, request
 import json
 import os
